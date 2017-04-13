b2b142d7d5f3ae786b7b81b5c3ad1c32ea76a1a3
zzsoszz <137573155@qq.com>
2017-04-13 23:13:46 +0800

    update

---
+++ app.js
@@ -0,0 +1,95 @@
+/*
+ *  jquery-boilerplate - v4.0.0
+ *  A jump-start for jQuery plugins development.
+ *  http://jqueryboilerplate.com
+ *
+ *  Made by Zeno Rocha
+ *  Under MIT License
+ */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;( function( $, window, document, undefined ) {
+
+	"use strict";
+
+		// undefined is used here as the undefined global variable in ECMAScript 3 is
+		// mutable (ie. it can be changed by someone else). undefined isn't really being
+		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+		// can no longer be modified.
+
+		// window and document are passed through as local variable rather than global
+		// as this (slightly) quickens the resolution process and can be more efficiently
+		// minified (especially when both are regularly referenced in your plugin).
+
+		// Create the defaults once
+		var pluginName = "fruitFall",
+			defaults = {
+				canvasSize: {
+					x:640,
+					y:690
+				}
+			};
+
+		// The actual plugin constructor
+		function Game ( element, options ) {
+			this.element = element;
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend( Game.prototype, {
+			init: function() {
+				//createig the pixi stage
+				this.stage=new PIXI.Stage(0x66FF99);
+
+				//create a root pixi object container
+				this.scene=new PIXI.DisplayObjectContainer();
+				this.stage.addChild(this.scene);
+
+				//create a render instance and set size  to default
+				this.renderer=PIXI.autoDetectRenderer(defaults.canvasSize.x,defaults.canvasSize.y);
+				
+				//add the renderer to the DOM
+				$(this.element).append(this.renderer.view);
+
+				//start animation ticker
+				window.requestAnimationFrame(this.animate.bind(this));
+				this.loadAssets();
+
+			},
+			animate: function() {
+				window.requestAnimationFrame(this.animate.bind(this));
+				this.renderer.render(this.stage);
+			},
+			loadAssets: function() {
+				var loader = PIXI.loader.add('assets/interface.json')
+							.on("progress", this.loadProgressHandler.bind(this));
+            	loader.load(this.onLoadAssets.bind(this));
+			},
+			onLoadAssets: function() {
+				var id=PIXI.loader.resources['assets/interface.json'].textures;
+				console.log(id);
+				var paperBG = new PIXI.Sprite(id["bg.png"]);
+            	this.scene.addChild(paperBG);
+			},
+			loadProgressHandler:function(loader, resource) {
+			  console.log("loading: " + resource.url); 
+			  console.log("progress: " + loader.progress + "%"); 
+			}
+		} );
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Game( this, options ) );
+				}
+			} );
+		};
+
+} )( jQuery, window, document );
